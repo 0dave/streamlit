a256523897033e74ff54c4e9e763787213ca4678
0dave <davidiling@yahoo.com>
2022-04-05 19:33:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ import numpy as np
 
 st.title('Hello from NY')
 
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
+
+
+
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
