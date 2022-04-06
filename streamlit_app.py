5f5c1271c731aa54e54f865ddfcb7f8718049d93
0dave <davidiling@yahoo.com>
2022-04-05 21:57:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Hello from NY')
+st.title('Uber pickups in NYC')
 
 dataframe = np.random.randn(10, 20)
 st.dataframe(dataframe)
 
-
-
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+# load data from the url above
+
+@st.cache
+def load_data(nrows):
 
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -25,4 +28,24 @@ data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+data_load_state.text("Done! (using st.cache)")
+
+st.subheader('Raw data')
+st.write(data)
+
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+
+st.bar_chart(hist_values)
+
+
+
+
+
+
+
+
+
